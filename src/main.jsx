e0bbdd1b454f6b9e6eef056699ff6ebde881512c
bosco11 <bosco11jico100@gmail.com>
2022-07-19 15:12:47 -0600

    useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { TodoApp } from './08-useReducer/TodoApp'
 //import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+//import { Padre } from './07-tarea-memo/Padre'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -16,6 +17,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Padre />
+    <TodoApp/>
   //</React.StrictMode>
 )
