a9c12d57c505d3306132d4ad5d6a54b1a16be775
bosco11 <bosco11jico100@gmail.com>
2022-07-19 11:42:39 -0600

    tarea

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHook } from './06-memos/CallbackHook'
+//import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -15,6 +16,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <CallbackHook />
+    <Padre />
   //</React.StrictMode>
 )
