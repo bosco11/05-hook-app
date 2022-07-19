86986f34818f3a33757f539a37d3ec70ef8c4e3e
bosco11 <bosco11jico100@gmail.com>
2022-07-19 09:16:25 -0600

    second commit

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,10 +1,27 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 export const Quote = ({autor,quote}) => {
+
+  const [bosxSize, setbosxSize] = useState({width:0,height:0});
+
+  const pRef = useRef()
+
+  useLayoutEffect(() => {
+    const {width,height} = pRef.current.getBoundingClientRect();
+    setbosxSize({width,height});
+  }, [quote])
+
+
   return (
     <>
-      <blockquote className="blockquote text-end">
-        <p className="mb-1">{quote}</p>
+      <blockquote className="blockquote text-end"
+      style={{display:"flex"}}
+      >
+        <p ref={pRef}className="mb-1">{quote}</p>
         <footer className="blockquote-footer">{autor}</footer>
       </blockquote>
+
+      <code>{JSON.stringify(bosxSize)}</code>
     </>
   );
 };
